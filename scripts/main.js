89f3b70164d335e587f610b3ae0b8880816ecc26
VictorAssunc <hugotex00@gmail.com>
2020-06-16 18:30:12 -0300

    implementa mais coisas

--- scripts/main.js
+++ scripts/main.js
@@ -2,7 +2,7 @@ const elements = document.querySelectorAll('[data-animate]');
 const animationClass = 'animation';
 
 function animateOnScroll() {
-	const pageTop = window.pageYOffset + (3 * window.innerHeight / 4);
+	const pageTop = window.pageYOffset + (2 * window.innerHeight / 5);
 	elements.forEach(function(element) {
 		if(pageTop > element.offsetTop) {
 			element.classList.add(animationClass);
@@ -16,5 +16,23 @@ function animateOnScroll() {
 if(elements.length) {
 	window.addEventListener('scroll', function() {
 		animateOnScroll();
-	})
+	});
+}
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
